b21781516433212c214739a80f1cbbc5ab8b0531
cgsg247 <alexatyukov30@gmail.com>
2026-06-23 10:42:01 +0300

    Settings THG files.

--- THG/rollup.config.js
+++ THG/rollup.config.js
@@ -1,19 +1,19 @@
 const resolve = require("@rollup/plugin-node-resolve");
 const commonjs = require("@rollup/plugin-commonjs");
-const terser = require('@rollup/plugin-terser');
+const terser = require("@rollup/plugin-terser");
 
 module.exports = {
-    input: "src/client/main.js",
-    output: {
-        file: 'dist/bundle.js',
-        format: "iife",
-        sourcemap: 'inline',
-    },
-    plugins: [
-        resolve({
-            browser: true,
-        }),
-        commonjs(),
-        //terser()
-    ],
+  input: "src/client/main.js",
+  output: {
+    file: "dist/bundle.js",
+    format: "iife",
+    sourcemap: "inline",
+  },
+  plugins: [
+    resolve({
+      browser: true,
+    }),
+    commonjs(),
+    terser(),
+  ],
 };
